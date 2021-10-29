22b81abddd5247bb6c2b78c69f74658a1f5a4952
Aleh Serhiyenia <Aleh_Serhiyenia@epam.com>
2021-10-29 18:26:39 +0400

    fix

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'b514e755bee248ed8992d4034d670433', // получите свой ключ, не используйте мой
+            apiKey: '', // получите свой ключ https://newsapi.org/
         });
     }
 }
