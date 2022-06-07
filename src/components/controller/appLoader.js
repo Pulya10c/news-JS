f769cc1dfbc2867fc1d57e0a7c8eb77ebad8a962
Bulation <insanbulat@gmail.com>
2022-06-07 18:52:42 +0300

    feat: add lifehack for correct work of deploy on gh-pages

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,7 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi.org/v2/', // для корректной работы деплоя на gh-pages замените этот url на https://nodenews.herokuapp.com/ 
+        {
             apiKey: '', // получите свой ключ https://newsapi.org/
         });
     }
