b16dc28c26bea4603b6869dbe4088eb26773415a
koviatsinets <koviatsinets@gmail.com>
2022-07-04 00:07:51 +0300

    feat: news module complete

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,38 +1,38 @@
+import { TopHeadlines } from '../../../types/index'
 import './news.css';
 
 class News {
-    draw(data) {
-        console.log(data)
+    draw(data: TopHeadlines[]): void { 
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
-
+        console.log(data)
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        news.forEach((item: TopHeadlines, idx: number) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
+            (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
                 item.urlToImage || 'img/news_placeholder.jpg'
             })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
+            (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent = item.author || item.source.name;
+           ( newsClone.querySelector('.news__meta-date') as HTMLElement).textContent = item.publishedAt
                 .slice(0, 10)
                 .split('-')
                 .reverse()
                 .join('-');
 
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            (newsClone.querySelector('.news__description-title') as HTMLElement).textContent = item.title;
+            (newsClone.querySelector('.news__description-source') as HTMLElement).textContent = item.source.name;
+            (newsClone.querySelector('.news__description-content') as HTMLElement).textContent = item.description;
+            (newsClone.querySelector('.news__read-more a') as HTMLElement).setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        (document.querySelector('.news') as HTMLElement).innerHTML = '';
+        (document.querySelector('.news') as HTMLElement).appendChild(fragment);
     }
 }
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,17 @@ export interface Source {
     language: string,
     country: string
 }
+
+export interface TopHeadlines {
+    author: string | null,
+    content: string | null,
+    description: string | null,
+    publishedAt: string,
+    source: {
+        id: string | null,
+        name: string | null,
+    },
+    title: string | null,
+    url: string,
+    urlToImage: string | null
+}
