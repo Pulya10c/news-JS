de8d5a183cbcada4f5019a7e2fb28ad06428a964
Alen Kazlovsky <alen-borisov@list.ru>
2022-07-04 10:43:45 +0300

    feat: customize webpack and ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,69 @@
 const path = require('path');
-const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8080,
+        contentBase: path.join(__dirname, 'public'),
     },
-    resolve: {
-        extensions: ['.js'],
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
+
+module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
+    entry: {
+        main: './src/index.ts',
     },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    module: {
+        rules: [{
+                test: /\.[tj]s$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ],
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
+        ...esLintPlugin(development),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new HtmlWebpackPlugin({ title: 'Hello World' }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'public' },
+            ],
         }),
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     ],
-};
-
-module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-
-    return merge(baseConfig, envConfig);
-};
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    ...devServer(development)
+});
