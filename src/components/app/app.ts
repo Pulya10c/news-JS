a2048b12c9d2ea504e15f3b2c8f72a452700cdd1
koviatsinets <koviatsinets@gmail.com>
2022-07-04 14:37:44 +0300

    fix: correct input value in webpack.config

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,10 +1,10 @@
-import { DrawNews, ResponseObj } from '../../types/index';
+import { Data, DrawNews, ResponseObj } from '../../types/index';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
-    controller: AppController
-    view: AppView
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -13,8 +13,9 @@ class App {
     start(): void {
         document
             .querySelector('.sources')!
-            .addEventListener('click', (e: Event) => 
-            this.controller.getNews(e, (data) => this.view.drawNews((data as unknown) as DrawNews)));
+            .addEventListener('click', (e: Event) =>
+                this.controller.getNews(e, (data) => this.view.drawNews((data as unknown) as DrawNews))
+            );
         this.controller.getSources((data) => this.view.drawSources(data as ResponseObj));
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
