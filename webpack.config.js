2fde4ed5d99774f240d68413f29d9eeecf365cd1
Alen Kazlovsky <alen-borisov@list.ru>
2022-07-04 14:36:51 +0300

    feat: add the link to RSschool & GitHub

--- webpack.config.js
+++ webpack.config.js
@@ -8,65 +8,64 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 
 const devServer = (isDev) => !isDev ? {} : {
-  devServer: {
-    open: true,
-    port: 8080,
-    contentBase: path.join(__dirname, 'public'),
-  },
+    devServer: {
+        open: true,
+        port: 8080,
+        contentBase: path.join(__dirname, 'public'),
+    },
 };
 
-const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+const esLintPlugin = (isDev) => isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })];
 
 module.exports = ({ development }) => ({
-  mode: development ? 'development' : 'production',
-  devtool: development ? 'inline-source-map' : false,
-  entry: {
-    main: './src/index.ts',
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext]',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]s$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
+    entry: {
+        main: './src/index.ts',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    module: {
+        rules: [{
+                test: /\.[tj]s$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ],
+    },
+    plugins: [
+        ...esLintPlugin(development),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, './src/img'),
+                to: path.resolve(__dirname, 'dist/img')
+            }],
+        }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     ],
-  },
-  plugins: [
-    ...esLintPlugin(development),
-    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new CopyPlugin({
-      patterns: [{
-        from: 'public',
-        noErrorOnMissing: true,
-      }],
-    }),
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  ...devServer(development)
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    ...devServer(development)
 });
