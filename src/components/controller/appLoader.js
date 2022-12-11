84d4b34d1d0df7fd9e3e2bd188a272a1a5df956c
Pavel Verbitsky <plov235@gmail.com>
2022-12-11 10:56:35 +0100

    feat: add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'cf8961f092204bd1ade66b82bff2aa47', // получите свой ключ https://newsapi.org/
         });
     }
 }
