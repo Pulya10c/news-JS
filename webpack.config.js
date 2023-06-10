310b1be434c3fc63e58dc4c0ef7a66e4bb5f972b
Ginezis <Ginezis25@gmail.com>
2023-06-10 23:31:03 +0300

    chore: add ts-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,35 +4,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
+	entry: path.resolve(__dirname, './src/index.js'),
+	mode: 'development',
+	module: {
+		rules:
+			[{
+				test: /\.ts$/i,
+				use: 'ts-loader'
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			],
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.json'],
+	},
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, '../dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './src/index.html'),
+			filename: 'index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+	const isProductionMode = mode === 'prod';
+	const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-    return merge(baseConfig, envConfig);
+	return merge(baseConfig, envConfig);
 };
