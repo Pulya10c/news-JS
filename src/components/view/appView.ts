4dd72532ff04e507ac3e5f017d35a1e088ad6b16
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-06-12 19:13:39 +0300

    refactor: add types to all variables on appView

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,18 @@
 import News from './news/news';
 import Sources from './sources/sources';
-import { ISource, INews } from '../../types';
+import { ISource, INews, INewsData, ISourceData } from '../../types';
 
 export class AppView {
-    public news = new News();
-    public sources = new Sources();
+    public news: News = new News();
+    public sources: Sources = new Sources();
 
-    drawNews(data: INews) {
-        const values = data?.articles ? data?.articles : [];
+    drawNews(data: INews | undefined): void {
+        const values: INewsData[] = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data: ISource) {
-        const values = data?.sources ? data?.sources : [];
+    drawSources(data: ISource | undefined): void {
+        const values: ISourceData[] = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
