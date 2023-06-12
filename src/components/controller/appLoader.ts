39de5b3a36206fc090bc184d5e4dbbfa56ac6a8f
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-06-12 12:57:29 +0300

    feat: add personal API key

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e5df1697aa2a4970846d1689e64c8846', // получите свой ключ https://newsapi.org/
         });
     }
 }
