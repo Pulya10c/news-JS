cecce9795f51f701ce63c404c27a2bc784dedf78
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-06-12 21:29:04 +0300

    refactor: add types to all variables on news file.

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,25 +1,40 @@
 import './news.css';
 import { INewsData } from '../../../types';
 
-class News {
-    draw(data: INewsData[]) {
-        const news = data.length >= 10 ? data.slice(0, 10) : data;
+enum Selectors {
+    ItemTemp = '#newsItemTemp',
+    News = '.news',
+    Item = '.news__item',
+    MetaPhoto = '.news__meta-photo',
+    MetaAuthor = '.news__meta-author',
+    MetaDate = '.news__meta-date',
+    DescriptionTitle = '.news__description-title',
+    DescriptionSource = '.news__description-source',
+    DescriptionContent = '.news__description-content',
+    ReadMore = '.news__read-more a',
+}
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
-        const newsWrapper = document.querySelector('.news');
+class News {
+    draw(data: INewsData[]): void {
+        const news: readonly INewsData[] = data.length >= 10 ? data.slice(0, 10) : data;
+
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement | null = document.querySelector(
+            Selectors.ItemTemp
+        ) as HTMLTemplateElement;
+        const newsWrapper: Element | null = document.querySelector(Selectors.News);
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
+            const newsClone = newsItemTemp?.content.cloneNode(true) as HTMLElement;
 
-            const photo = newsClone.querySelector('.news__meta-photo') as HTMLImageElement;
-            const author = newsClone.querySelector('.news__meta-author');
-            const date = newsClone.querySelector('.news__meta-date');
-            const title = newsClone.querySelector('.news__description-title');
-            const source = newsClone.querySelector('.news__description-source');
-            const content = newsClone.querySelector('.news__description-content');
+            const photo: HTMLImageElement | null = newsClone.querySelector(Selectors.MetaPhoto);
+            const author: Element | null = newsClone.querySelector(Selectors.MetaAuthor);
+            const date: Element | null = newsClone.querySelector(Selectors.MetaDate);
+            const title: Element | null = newsClone.querySelector(Selectors.DescriptionTitle);
+            const source: Element | null = newsClone.querySelector(Selectors.DescriptionSource);
+            const content: Element | null = newsClone.querySelector(Selectors.DescriptionContent);
 
-            if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
+            if (idx % 2) newsClone.querySelector(Selectors.Item)?.classList.add('alt');
 
             if (photo) {
                 photo.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
@@ -45,7 +60,7 @@ class News {
                 content.textContent = item.content;
             }
 
-            newsClone.querySelector('.news__read-more a')?.setAttribute('href', item.url);
+            newsClone.querySelector(Selectors.ReadMore)?.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
