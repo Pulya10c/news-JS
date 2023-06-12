114a19404da02179f2a5f47ae85e4ce6ef8b87dd
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2023-06-12 19:47:31 +0300

    refactor: add types to all variables on sources and enum Selectors

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,25 +1,39 @@
 import './sources.css';
 import { ISourceData } from '../../../types';
 
+enum Selectors {
+    Item = '.source__item',
+    ItemTemp = '#sourceItemTemp',
+    ItemName = '.source__item-name',
+    DataId = 'data-source-id',
+    Sources = '.sources',
+}
+
 class Sources {
     draw(data: ISourceData[]) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector(
+            Selectors.ItemTemp
+        ) as HTMLTemplateElement;
+
+        if (!sourceItemTemp) {
+            throw new Error('Template element not found');
+        }
 
         data.forEach((item) => {
             const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
-            const name = sourceClone.querySelector('.source__item-name');
+            const name: HTMLElement | null = sourceClone.querySelector(Selectors.ItemName);
 
             if (name) {
                 name.textContent = item.name;
             }
 
-            sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+            sourceClone.querySelector(Selectors.Item)?.setAttribute(Selectors.DataId, item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources')?.append(fragment);
+        document.querySelector(Selectors.Sources)?.append(fragment);
     }
 }
 
