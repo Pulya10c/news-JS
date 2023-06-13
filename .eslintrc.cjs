597d2938dddcdfc426d45b4158ce27a6767cefda
alex.pranevich <freennnn@gmail.com>
2023-06-13 16:32:37 -0700

    fix: remove eslint-plugin-prettier as it conflicted with eslint-configure-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,20 @@
 /* eslint-env node */
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    root: true,
-    rules: {},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+  },
 };
