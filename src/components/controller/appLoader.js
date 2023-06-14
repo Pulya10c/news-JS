aa5f082c03c96b06a8c272370b60b9687135ddbb
alex.pranevich <freennnn@gmail.com>
2023-06-13 21:37:30 -0700

    fix: change News API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader.js';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '', // получите свой ключ https://newsapi.org/
+      apiKey: 'abf0432f07594bf18c94c89641b3f229', // получите свой ключ https://newsapi.org/
     });
   }
 }
