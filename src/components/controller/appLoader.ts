5f74f92697b664b623e03d322e805a90df4563c6
alex.pranevich <freennnn@gmail.com>
2023-06-23 10:58:03 +0200

    fix: change proxy  for newAPI deployed

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,9 +2,14 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    // super('https://newsapi.org/v2/', {
+    //   apiKey: 'abf0432f07594bf18c94c89641b3f229', // получите свой ключ https://newsapi.org/
+    // });
+
+    super('https://rss-news-api.onrender.com/', {
       apiKey: 'abf0432f07594bf18c94c89641b3f229', // получите свой ключ https://newsapi.org/
     });
+
     // super('https://news-proxy.spanb4.shop/', {
     //   apiKey: 'abf0432f07594bf18c94c89641b3f229', // получите свой ключ https://newsapi.org/
     // });
